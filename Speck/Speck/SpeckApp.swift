ff7ef1fa89ecba359b2b45ec4dfa51b858804813
Jari Zwarts <jari@oberon.nl>
2023-04-26 23:15:52 +0200

    app, bridge setup

---
+++ Speck/Speck/SpeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeckApp.swift
+//  Speck
+//
+//  Created by Jari on 26/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SpeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
